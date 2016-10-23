1b163257158ee761c8f5ad3ebb0c499bb107943b
Fabiano Nalin <fnalin@msn.com>
2016-10-23 15:19:26 -0200

    Add Demo Rendering Views

---
+++ 01 - Started/07 - Rendering Views/RenderingViews/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RenderingViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cliente()
+        {
+            return new ObjectResult(new
+            {
+                Id = 1,
+                Nome = "Fabiano Nalin"
+            });
+        }
+    }
+}
